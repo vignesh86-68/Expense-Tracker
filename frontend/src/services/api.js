a7f97170dacd0bc417c37b6be280c410eb813607
vignesh86-68 <vigneshsakthivel8668@gmail.com>
2026-04-19 02:23:31 +0530

    Fix API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
+const API = axios.create({
+  baseURL: 'https://expense-tracker-rkt8.onrender.com/api'
+});
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
